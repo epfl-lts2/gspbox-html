98daace0b7a1ca0b82efd2e650ad3ef38a888e71
Nathanael Perraudin <nperraud@users.noreply.github.com>
2017-10-11 10:48:11 +0200

    remove notes

--- notes/description.php
+++
@@ -1,3 +0,0 @@
-<h2>GSPBox Notes</h2>
-<p>The GSPBox notes is a collection of information relating to the GSPBox. An GSPBox note may be everything from a ph.d. thesis, a preprint of a journal paper to a note describing the design of a function etc.</p>
-   <p>Contributions are welcome, see the very first note for details.</p>
